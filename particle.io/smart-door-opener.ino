dbb47cc3ffbfcbbc22506dde007518f3fc8ead16
potpiejimmy <t.liese@outlook.com>
2017-06-04 10:22:50 +0200

    perform async buzz on core

--- particle.io/smart-door-opener.ino
+++ particle.io/smart-door-opener.ino
@@ -3,21 +3,27 @@
 int buzzer = D7;
 int BUZZING_TIME = 2000;
 
+int doBuzz = 0;
+
 void setup()
 {
    pinMode(buzzer, OUTPUT);
-   Spark.function("buzz", doBuzzing);
+   Spark.function("buzz", triggerBuzz);
    digitalWrite(buzzer, LOW);
 }
 
 void loop()
 {
+    if (doBuzz) {
+        digitalWrite(buzzer, HIGH);
+        delay(BUZZING_TIME);
+        digitalWrite(buzzer, LOW);
+        doBuzz = 0;
+    }
 }
 
-int doBuzzing(String command)
+int triggerBuzz(String command)
 {
-    digitalWrite(buzzer, HIGH);
-    delay(BUZZING_TIME);
-    digitalWrite(buzzer, LOW);
+    doBuzz = 1;
     return 1;
 }
